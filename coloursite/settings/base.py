a82152e053b8a1ef471b85b073ec7bdceb391ff5
Karl Hobley <karl@torchbox.com>
2014-11-04 14:23:21 +0000

    Corrected static files directory setting

--- coloursite/settings/base.py
+++ coloursite/settings/base.py
@@ -1,7 +1,7 @@
 # Django settings for coloursite project.
 
 import os
-BASE_DIR = os.path.dirname(__file__)
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -64,7 +64,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = os.path.join(BASE_DIR + '/static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
