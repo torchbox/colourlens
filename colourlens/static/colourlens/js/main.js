c911c4c2a488e4b57a487ed9c6c46769ce43c3e2
James Morris <jamesm@torchbox.com>
2014-12-02 10:13:31 +0000

    Front end work and layout of the design. Updated gitignore to remove CACHE from being added

--- colourlens/static/colourlens/js/main.js
+++ colourlens/static/colourlens/js/main.js
@@ -1,19 +1,52 @@
 $(function() {
   
+    //Set the colour strip heights
+    function setColourPickerHeight() {
+        var winHeight = $(window).height();
+        var headerHeight = $('header.site-header').height();
+        $('#colour-search li').each(function( index ) {
+            //Make a random number
+            var randNum = Math.floor(Math.random()*(3500-1000+1)+1000);
+            $(this).animate({height: winHeight - headerHeight}, randNum);
+        });
+    }
 
+    setColourPickerHeight();
 
-  if ( !$('body').hasClass('ui-status-splash') ) {
-    //Creates colour highligher on hover of a colour
-    $('ul.colours li').hover(function() {
-      //Reset any cloned items
-      $('ul.colours li.cloned').remove();
-      var left = $(this).offset().left;
-      var cloned = $(this).clone();
-      var width = $(this).width();
-      $(cloned).addClass('cloned').css('position','absolute').css('left', left);
-      $('ul.colours').append(cloned);
+    //Init jQuery UI tabs
+    $('.colour-selector').tabs();
+    
+      //Creates colour highligher on hover of a colour
+    // $('ul.colours li').hover(function() {
+    //     if ( $('body').hasClass('ui-status-picker') ) {
+    //         var left = $(this).offset().left;
+    //         var cloned = $(this).clone();
+    //         var width = $(this).width();
+    //         //Set the selected status
+    //         //But remove any old ones first
+    //         $('ul.colours li').removeClass('selected');
+    //         $(this).addClass('selected');
+    //         //Reset any cloned items
+    //         $('ul.colours li.cloned').remove();
+    //         $(cloned).addClass('cloned').css('position','absolute').css('left', left);
+    //         $('ul.colours').append(cloned);
+    //     }
+    // });
+
+    //Fade out the mag glass
+    $('.ui-status-splash .mag-glass h2').delay(2000).fadeOut(1000);
+    $('.ui-status-splash .mag-glass').delay(2500).fadeOut(1400, function() {
+        $('body').removeClass('ui-status-splash').addClass('ui-status-picker');
     });
-  }
+
+    //Progress artwork picker state
+    $(document).on('click','.colours li.cloned a',function() {
+        $('body').removeClass('ui-status-picker').addClass('ui-status-artwork-pick');
+        $('#colour-search li.cloned').fadeOut(100).remove();
+        $('#colour-search li').each(function( index ) {
+            $(this).animate({height: '80px' }, 1200);
+        });
+    });
 
 
 });
