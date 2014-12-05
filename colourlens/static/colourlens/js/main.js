87caa4bb6aef9f3c0e1b14272c08e333af1a32a1
James Morris <jamesm@torchbox.com>
2014-12-05 17:34:16 +0000

    Ironing out the UI so it works smoothly

--- colourlens/static/colourlens/js/main.js
+++ colourlens/static/colourlens/js/main.js
@@ -1,10 +1,13 @@
 $(function() {
+
+    //Init jQuery UI tabs
+    $('.colour-selector').tabs();
   
     //Set the colour strip heights
     function setColourPickerHeight() {
         var winHeight = $(window).height();
         var headerHeight = $('header.site-header').height();
-        $('#colour-search li').each(function( index ) {
+        $('.ui-tabs-panel li').each(function( index ) {
             //Make a random number
             var randNum = Math.floor(Math.random()*(3500-1000+1)+1000);
             $(this).animate({height: winHeight - headerHeight}, randNum);
@@ -12,26 +15,23 @@ $(function() {
     }
 
     setColourPickerHeight();
-
-    //Init jQuery UI tabs
-    $('.colour-selector').tabs();
-    
-      //Creates colour highligher on hover of a colour
-    // $('ul.colours li').hover(function() {
-    //     if ( $('body').hasClass('ui-status-picker') ) {
-    //         var left = $(this).offset().left;
-    //         var cloned = $(this).clone();
-    //         var width = $(this).width();
-    //         //Set the selected status
-    //         //But remove any old ones first
-    //         $('ul.colours li').removeClass('selected');
-    //         $(this).addClass('selected');
-    //         //Reset any cloned items
-    //         $('ul.colours li.cloned').remove();
-    //         $(cloned).addClass('cloned').css('position','absolute').css('left', left);
-    //         $('ul.colours').append(cloned);
-    //     }
-    // });
+   
+    // Creates colour highligher on hover of a colour
+    $('ul.colours li').hover(function() {
+        if ( $('body').hasClass('ui-status-picker') ) {
+            var left = $(this).offset().left;
+            var cloned = $(this).clone();
+            var width = $(this).width();
+            //Set the selected status
+            //But remove any old ones first
+            $('ul.colours li').removeClass('selected');
+            $(this).addClass('selected');
+            //Reset any cloned items
+            $('ul.colours li.cloned').remove();
+            $(cloned).addClass('cloned').css('position','absolute').css('left', left);
+            $('ul.colours').append(cloned);
+        }
+    });
 
     //Fade out the mag glass
     $('.ui-status-splash .mag-glass h2').delay(2000).fadeOut(1000);
@@ -46,6 +46,8 @@ $(function() {
         $('#colour-search li').each(function( index ) {
             $(this).animate({height: '80px' }, 1200);
         });
+        var hex = $(this).data('hex-value');
+        $('.colours').find('li.selected:not(.cloned) a[data-hex-value="'+hex+'"]').click();
     });
 
 
